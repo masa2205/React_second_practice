05df2e7ee4d3c933f5e3187e3c77e1d9890a29c5
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-27 23:04:57 +0900

    class component と function component の違い

---
+++ src/Blog.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article";
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <Article />
+        <Article />
+      </>
+    );
+  }
+}
+
+export default Blog;
