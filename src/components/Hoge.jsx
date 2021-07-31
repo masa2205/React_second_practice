11e4bf440e41e20852a3754f75021942c8fc19ae
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-31 10:19:11 +0900

    モジュールについて

---
+++ src/components/Hoge.jsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+export default class Hoge extends React.Component {
+  render() {
+    return <h2>Hogeeeee</h2>;
+  }
+}
